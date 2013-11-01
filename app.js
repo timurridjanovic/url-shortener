aee350018b771221100f9b2ccd6e69a98a599148
timurridjanovic <timur.ridjanovic@gmail.com>
2013-11-01 19:34:06 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var url = require('url');
+var validateUrl = require('./public/js/bitly-clone');
+
+var app = express();
+//added
+var cons = require('consolidate');
+//
+
+//added
+app.engine('html', cons.swig);
+//
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+//get requests
+app.get('/', routes.index);
+
+//post requests
+app.post('/', function(req, res) {
+    var url = req.body.shortener;
+    validateUrl(url, req, res);
+});
+
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ public/js/bitly-clone.js
@@ -0,0 +1,27 @@
+var ValidateUrl = function(url, req, res) {
+    var regex = new RegExp("^(http|https|ftp)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+))*$");
+    
+    if (regex.test(url)) {
+        Shorten(url);
+        
+    }
+    else {
+        var error = "Please enter a valid URL";
+        console.log(error);
+        res.render('index', {error:error});
+    }
+    
+}
+
+var Shorten = function(url) {
+    this.urlDict = {};
+    var timer = new Date().getTime();
+    
+    console.log(timer);
+    
+    var shortenedUrl = 
+    this.urlDict[''] = 'hello';
+}
+
+module.exports = ValidateUrl;
+
